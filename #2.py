f41bcb843452d5bc2fac985976a84c69c1dcfef9
aditya-pathak-05 <537.adityapathak@gmail.com>
2026-08-28 15:39:52 +0530

    sum of N  no.s

---
+++ #2.py
@@ -0,0 +1,8 @@
+def sum():
+   n= int(input("ENTER HOW MANY NO.S YOU WANT TO ADD"))
+   a=0
+   for i in range(0,n):
+       b=float(input("ENTER A NO."))
+       a=a+b
+   print("SUM OF ALL NO'S ARE", a)
+sum() 
