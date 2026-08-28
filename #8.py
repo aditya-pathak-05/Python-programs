5a071dc18ad3df2d7762540f6c4e44ae0910ce6f
aditya-pathak-05 <537.adityapathak@gmail.com>
2026-08-28 17:43:53 +0530

    Sum of a series [ 1+x+x**2+x**3----x*n]

---
+++ #8.py
@@ -0,0 +1,8 @@
+def sumofnnos():
+    a=int(input("enter no. of natural nos"))
+    s=0
+    for i in range(0,a):
+        b=int(input("enter a no."))
+        s=s+b
+    print("sum is",s)
+sumofnnos()    
