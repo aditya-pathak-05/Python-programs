79701eb3335a94402f6f8123fdcc6f034cf56b76
aditya-pathak-05 <537.adityapathak@gmail.com>
2026-08-28 15:54:05 +0530

    Area and Perimeter of rectangle

---
+++ #3.py
@@ -0,0 +1,6 @@
+def measure():
+    a=float(input("enter lenght of rect(in cms)"))
+    b=float(input("enter breadth of rect(in cms)"))
+    print("PERIMETER OF RECT IS", 2*(a+b),"cm")
+    print("AREA OF RECT IS",a*b,"sq. cm")
+measure()    
