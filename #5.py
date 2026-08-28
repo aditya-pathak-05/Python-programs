87ca90bf0ff4dd351019fb98e3f75bd762a47d9d
aditya-pathak-05 <537.adityapathak@gmail.com>
2026-08-28 16:21:10 +0530

    Even Or Odd

---
+++ #5.py
@@ -0,0 +1,9 @@
+def evenorodd():
+    a=input('enter a no.')
+    if a.isdigit():
+        if int(a)%2==0:
+            print("ENTERED NO. IS A EVEN NO.")
+        else:
+            print("ENTERED NO. IS A ODD NO.")
+    else:
+        print("ERROR: PLEASE ENTER A INTEGER")
